f85465b999b8b005e1f27c5afcf3f7948c2fab3d
hominhduc18 <hoduc092@gmail.com>
2023-02-12 14:39:56 +0700

    main

--- app/src/main/java/com/example/userwarranty/Controller/dien.java
+++ app/src/main/java/com/example/userwarranty/Controller/dien.java
@@ -1,15 +1,40 @@
 package com.example.userwarranty.Controller;
 
+import android.annotation.SuppressLint;
+import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.userwarranty.R;
+import com.example.userwarranty.fragment.trangchuFragment;
 
 public class dien extends AppCompatActivity {
+     ImageView back ;
+    trangchuFragment trangchuFragments;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.information_dien);
+
+        back = findViewById(R.id.img_dien);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                trangchuFragments = new trangchuFragment();
+//                FragmentTransaction transaction =  trangchuFragments.beginTransaction();
+//                transaction.addToBackStack(null);
+//                transaction.commit();
+
+
+            }
+        });
+
     }
+
 }
--- app/src/main/java/com/example/userwarranty/fragment/trangchuFragment.java
+++ app/src/main/java/com/example/userwarranty/fragment/trangchuFragment.java
@@ -1,8 +1,11 @@
 package com.example.userwarranty.fragment;
 
+import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Handler;
@@ -12,6 +15,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.example.userwarranty.Controller.dien;
 import com.example.userwarranty.Model.photo;
 import com.example.userwarranty.R;
 import com.example.userwarranty.adapter.photoAdapter;
@@ -27,22 +31,46 @@ import me.relex.circleindicator.CircleIndicator;
  * A simple {@link Fragment} subclass.
  */
 public class trangchuFragment extends Fragment {
-    private ViewPager viewPager;
-    private CircleIndicator circleIndicator;
-    private photoAdapter PhotoAdapter;
-    // auto
-    private List<photo> mListPhoto;
-    private Timer mtimer;
+
+    private ImageView img_dien, img_dienlanh, img_ongnc, img_it, img_phone,img_NL,img_fixnha,img_vesinh,img_khac;
+    private dien dienActivity;
     private View view;
 
+    public void init(){
+        img_dien = view.findViewById(R.id.img_dien);
+        img_dienlanh = view.findViewById(R.id.img_dienlanh);
+        img_ongnc = view.findViewById(R.id.img_ongnc);
+        img_it = view.findViewById(R.id.img_IT);
+        img_phone = view.findViewById(R.id.img_phone);
+        img_NL = view.findViewById(R.id.img_nangluong);
+        img_fixnha = view.findViewById(R.id.img_suanha);
+        img_vesinh = view.findViewById(R.id.img_vesinh);
+        img_khac = view.findViewById(R.id.img_khac);
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_trangchu, container, false);
+        init();
+        img_dien.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                dienActivity = new dien();
+//                FragmentTransaction transaction =  dienActivity.getSupportFragmentManager().beginTransaction();
+//                transaction.addToBackStack(null);
+//                transaction.commit();
 
+                Intent intent = new Intent(getActivity(),dien.class);
+                startActivity(intent);
+            }
+        });
 
         return view;
 
+
     }
+
+
+
 }
